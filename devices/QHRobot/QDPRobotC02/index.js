415e6be8fc8b62f9e11561757b7acf6ed0e5c603
ArthurZheng <869705086@qq.com>
2022-07-03 20:06:44 +0800

    update qdp esp32 device

--- devices/QHRobot/QDPRobotC02/index.js
+++ devices/QHRobot/QDPRobotC02/index.js
@@ -1,32 +1,48 @@
 const QDPRobotC02 = formatMessage => ({
     name: 'QDP RobotC02',
-    deviceId: 'QDPRobotC02_arduinoEsp32',
+    deviceId: 'QDPRobotC02_esp32',
     manufactor: 'QDP Robot',
     learnMore: 'https://qdprobot.taobao.com',
+    typeList: ['arduino', 'microPython'],
     iconURL: 'asset/QDPRobotC02.png',
     description: formatMessage({
         id: 'QDPRobotC02.description',
         default: 'QDP robot C02.'
     }),
-    disabled: false,
-    bluetoothRequired: false,
-    serialportRequired: true,
-    defaultBaudRate: '9600',
     pnpidList: [
         'USB\\VID_10C4&PID_EA60', // CP2102
         'USB\\VID_1A86&PID_7523' // CH340
     ],
-    internetConnectionRequired: false,
-    launchPeripheralConnectionFlow: true,
-    useAutoScan: false,
     connectionIconURL: 'asset/QDPRobotC02-illustration.svg',
     connectionSmallIconURL: 'asset/QDPRobotC02-small.svg',
-    programMode: ['upload'],
-    programLanguage: ['block', 'cpp'],
-    tags: ['microbit', 'kit'],
+    programLanguage: ['block', 'c', 'cpp', 'microPython'],
+    tags: ['kit'],
     deviceExtensions: ['QDPRobotC02'],
-    deviceExtensionsCompatible: 'arduinoEsp32',
     helpLink: 'http://www.qdprobot.com/'
 });
 
-module.exports = QDPRobotC02;
+const QDPRobotC02Arduino = formatMessage => {
+    const device = QDPRobotC02(formatMessage);
+    device.defaultBaudRate = '9600';
+    device.deviceId = 'QDPRobotC02_arduinoEsp32';
+    device.programMode = ['upload'];
+    deviceExtensionsCompatible = 'arduinoEsp32',
+    device.hide = true;
+    return device;
+}
+
+const QDPRobotC02MicroPython = formatMessage => {
+    const device = QDPRobotC02(formatMessage);
+    device.defaultBaudRate = '115200';
+    device.deviceId = 'QDPRobotC02_microPythonEsp32';
+    device.programMode = ['upload'];
+    deviceExtensionsCompatible = 'microPythonEsp32',
+    device.hide = true;
+    return device;
+}
+
+module.exports = formatMessage => ([
+    QDPRobotC02(formatMessage),
+    QDPRobotC02Arduino(formatMessage),
+    QDPRobotC02MicroPython(formatMessage)
+]);
